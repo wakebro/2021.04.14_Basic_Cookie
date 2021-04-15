020c76319d33ec47da9a639e398d401372bbd3ed
wake <wakebro119@naver.com>
2021-04-15 21:32:43 +0900

    express-session setup

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # 2021.04.14_Basic_Cookie
 
-Node JS를 이용한 기초 쿠키
+Node JS를 이용한 기초 쿠키, 세션
 
 ## 기술
 
 - Node JS
 - Cookie
+- Seesion
---
+++ nodejs/express-session.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var parseurl = require("parseurl");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
+
+var app = express();
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore(),
+  })
+);
+
+app.get("/", function (req, res, next) {
+  if (req.session.num === undefined) {
+    req.session.num = 1;
+  } else {
+    req.session.num += 1;
+  }
+  res.send(`num:${req.session.num}`);
+});
+
+app.listen(3000, () => {
+  console.log("3000!");
+});
